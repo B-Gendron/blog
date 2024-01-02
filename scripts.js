553b362e51c19c109e6983002242a903c0d2841d
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2024-01-02 17:42:50 +0100

    working version + prepare language selection

--- scripts.js
+++ scripts.js
@@ -6,3 +6,27 @@ function showMessage() {
 function navigateToPost(postUrl) {
     window.location.href = postUrl;
 }
+
+function goBackToMainPage() {
+    console.log("Function called");
+    window.location.href = "../index.html";
+}
+
+// Function to change language
+function changeLanguage(language) {
+    if (language === 'en') {
+        document.getElementById('content-en').style.display = 'block';
+        document.getElementById('content-fr').style.display = 'none';
+    } else if (language === 'fr') {
+        document.getElementById('content-en').style.display = 'none';
+        document.getElementById('content-fr').style.display = 'block';
+    }
+}
+
+// Function to navigate to a post
+function navigateToPost(postUrl) {
+    // Add your navigation logic here
+}
+
+// Initial language setting (English)
+changeLanguage('en');
