129ad79bf14faaf7e39cac9440810b35d8722aae
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2024-01-02 16:22:17 +0100

    first working version

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// a dummy test function
+function showMessage() {
+    alert('Hello, this is a message from scripts.js!');
+}
+
+function navigateToPost(postUrl) {
+    window.location.href = postUrl;
+}
